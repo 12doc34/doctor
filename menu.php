f254563479bfe2980b7c018dff3a3518b8e428fd
yaffa95 <33422029+yaffa95@users.noreply.github.com>
2019-08-11 14:24:45 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,18 +2,18 @@
 
 if (isset($_SESSION['Logged']) && $username == "doc") {
     echo "doctor";
-    header("Location:appointments.php");
-    header("Location:doctor.php");
-    header("Location:calendar.php");
-    header("Location:noinvs.php");
-    header("Location:staff.html");
-    header("Location:logout.php");
+    <html><a href="appointments.php">appointments</a></html>
+    <html><a href="doctor.php">doctor</a></br></html>
+    <html><a href="calendar.php">calendar</a></br></html>
+    <html><a href="noinvs.php">no invoice</a></br></html>
+    <html><a href="staff.html">staff</a></br></html>
+    <html><a href="logout.html">logout</a></br></html>
 } 
 else if (isset($_SESSION['Logged']) && $username == "esc") {
     echo "secretary";
-    header("Location:calendar.php");
-    header("Location:appointments.php");
-    header("Location:noinvs.php");
-    header("Location:logout.php");
+     <html><a href="calendar.php">calendar</a></html>
+    <html><a href="appointments.php">appointments</a></br></html>
+    <html><a href="noinvs.php">no invoice</a></br></html>
+    <html><a href="logout.html">logout</a></br></html>
 } 
 ?>
