0d00e8e817306498e2c392a607948b7f7329a2d0
yaffa95 <33422029+yaffa95@users.noreply.github.com>
2019-08-20 19:43:07 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,19 +1,32 @@
 <?php
-
-if (isset($_SESSION['Logged']) && $username == "doc") {
+session_start();
+if(!isset($_SESSION['username']))
+{
+    header('location':login.php);
+        exit();
+}
+if ($_SESSION['username'] == "doc") {
     echo "doctor";
-    <html><a href="appointments.php">appointments</a></html>
-    <html><a href="doctor.php">doctor</a></br></html>
-    <html><a href="calendar.php">calendar</a></br></html>
-    <html><a href="noinvs.php">no invoice</a></br></html>
-    <html><a href="staff.html">staff</a></br></html>
-    <html><a href="logout.html">logout</a></br></html>
+    echo "<html>";
+    echo "<body>";
+    echo "<a href=\"appointments.php\">appointments</a></br>";
+    echo "<a href=\"doctor.php\">doctor</a></br>";
+    echo "<a href=\"calendar.php\">calendar</a></br>";
+    echo "<a href=\"noinvs.php\">no invoice</a></br>";
+    echo "<a href=\"staff.html\">staff</a></br>";
+    echo "<a href=\"logout.html\">logout</a>";
+    echo "</body>";
+    echo "</html>";
 } 
-else if (isset($_SESSION['Logged']) && $username == "esc") {
+else if ($_SESSION['username'] == "sec") {
     echo "secretary";
-     <html><a href="calendar.php">calendar</a></html>
-    <html><a href="appointments.php">appointments</a></br></html>
-    <html><a href="noinvs.php">no invoice</a></br></html>
-    <html><a href="logout.html">logout</a></br></html>
+    echo "<html>";
+    echo "<body>";
+    echo "<a href=\"calendar.php\">calendar</a>";
+    echo "<a href=\"appointments.php\">appointments</a></br>";
+    echo "<a href=\"noinvs.php\">no invoice</a></br>";
+    echo "<a href=\"logout.html\">logout</a></br>";
+    echo "</body>";
+    echo "</html>";
 } 
 ?>
